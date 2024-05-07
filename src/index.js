ba34896f65f923961eb4c8454008e766a66415a4
DianiMor <diana.3mol@gmail.com>
2024-05-06 20:03:32 -0500

    ruta app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Importa App como el componente predeterminado desde './App'
+import App from 'components/App'; // Importa App como el componente predeterminado desde './App'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
